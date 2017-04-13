bdf8f7bbf8e96e5a9da518f9e6a549b0d8e0bfb5
yoojeen <publictomd@gmail.com>
2017-04-13 21:19:17 +0500

    ok
    ok

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    private static Program pr;
+
+    public static void main(String[] args) {
+        if ( pr == null ) pr = new Program();
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,11 @@
+/**
+ * Created by n0mad on 13/04/2017.
+ */
+public class Program {
+
+
+    public Program()
+    {
+        System.out.println ("Hello!");
+    }
+}
